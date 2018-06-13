26d89626baa457092da5e1292b57105b5ae43399
Rebzzel <rebzzel@gmail.com>
2018-06-13 20:03:34 +0300

    Add single instance implementation

--- PicPrompt/App.xaml.cs
+++ PicPrompt/App.xaml.cs
@@ -1,14 +1,28 @@
-﻿using System;
+﻿using Microsoft.Shell;
+using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace PicPrompt
 {
-    public partial class App : Application
+    public partial class App : Application, ISingleInstanceApp
     {
+        [STAThread]
+        public static void Main()
+        {
+            if (SingleInstance<App>.InitializeAsFirstInstance("PicPrompt"))
+            {
+                App app = new App();
+                app.InitializeComponent();
+                app.Run(new MainWindow());
+
+                SingleInstance<App>.Cleanup();
+            }
+        }
+
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            return true;
+        }
     }
 }
