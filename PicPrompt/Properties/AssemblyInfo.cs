56750781720ab897f893b1dd2af25ac3648f20b6
Rebzzel <rebzzel@gmail.com>
2018-06-13 19:07:24 +0300

    Add .gitignore, .gitattributes and project files

---
+++ PicPrompt/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Windows;
+
+[assembly: AssemblyTitle("PicPrompt")]
+[assembly: AssemblyProduct("PicPrompt")]
+[assembly: AssemblyDescription("Fast, simple and lightweight image viewer.")]
+[assembly: AssemblyCopyright("© 2018 Rebzzel")]
+[assembly: AssemblyVersion("1.00")]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
+)]
