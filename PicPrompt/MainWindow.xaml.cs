43043d9202c69a056762f439b2925eef33ded19e
Rebzzel <rebzzel@gmail.com>
2018-06-14 22:52:19 +0300

    Add images, controls and some functional, move title buttons from styles to window

--- PicPrompt/MainWindow.xaml.cs
+++ PicPrompt/MainWindow.xaml.cs
@@ -8,5 +8,22 @@ namespace PicPrompt
         {
             InitializeComponent();
         }
+
+        private void Close_Click(object sender, RoutedEventArgs e) => Close();
+
+        private void MaximizeOrRestore_Click(object sender, RoutedEventArgs e)
+        {
+            switch (WindowState)
+            {
+                case WindowState.Normal:
+                    WindowState = WindowState.Maximized;
+                    break;
+                case WindowState.Maximized:
+                    WindowState = WindowState.Normal;
+                    break;
+            }
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
     }
 }
