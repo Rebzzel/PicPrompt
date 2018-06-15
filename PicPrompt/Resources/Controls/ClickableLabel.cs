66a576485213098f8caf8874fc892157e7edb4f5
Rebzzel <rebzzel@gmail.com>
2018-06-15 21:18:31 +0300

    Add controls, label styles and ClickableLabel control

---
+++ PicPrompt/Resources/Controls/ClickableLabel.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace PicPrompt.Resources.Controls
+{
+    class ClickableLabel : Label
+    {
+        public static readonly RoutedEvent ClickEvent = ButtonBase.ClickEvent.AddOwner(typeof(ClickableLabel));
+
+        public event RoutedEventHandler Click
+        {
+            add => AddHandler(ClickEvent, value);
+            remove => RemoveHandler(ClickEvent, value);
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseDown(e);
+
+            CaptureMouse();
+        }
+
+        protected override void OnMouseUp(MouseButtonEventArgs e)
+        {
+            base.OnMouseUp(e);
+
+            if (IsMouseCaptured)
+            {
+                ReleaseMouseCapture();
+
+                if (IsMouseOver)
+                    RaiseEvent(new RoutedEventArgs(ClickEvent, this));
+            }
+        }
+    }
+}
