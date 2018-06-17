c756248aba00e3633e92a4114c6aadb9176c0424
Rebzzel <rebzzel@gmail.com>
2018-06-17 22:36:00 +0300

    Move controls to StackPanels, add some functional

--- PicPrompt/MainWindow.xaml.cs
+++ PicPrompt/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using ImageMagick;
+using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Media.Animation;
 
 namespace PicPrompt
 {
@@ -30,6 +34,37 @@ namespace PicPrompt
         private void Minimize_Click(object sender, RoutedEventArgs e)
         {
             WindowState = WindowState.Minimized;
+        }
+
+        private void OpenImage(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            
+            if (dialog.ShowDialog() is true)
+            {
+                using (MagickImage image = new MagickImage(dialog.FileName))
+                {
+                    Separator2.Visibility = Visibility.Visible;
+                    NameLbl.Visibility = Visibility.Visible;
+                    NameLbl.Content = $"{dialog.SafeFileName}";
+                    Separator3.Visibility = Visibility.Visible;
+                    SizeLbl.Visibility = Visibility.Visible;
+                    SizeLbl.Content = $"{image.Width} x {image.Height}";
+
+                    NoneContentGrid.Visibility = Visibility.Collapsed;
+
+                    Viewer.Width = image.Width;
+                    Viewer.Height = image.Height;
+                    Viewer.Source = image.ToBitmapSource();
+
+                    if (Toolbar.Visibility == Visibility.Collapsed)
+                    {
+                        Toolbar.Visibility = Visibility.Visible;
+                        Toolbar.BeginAnimation(MarginProperty, new ThicknessAnimation(new Thickness(0, 0, 0, 0), new Thickness(0, 0, 0, 30), new TimeSpan(0, 0, 0, 0, 700)));
+                        Toolbar.BeginAnimation(OpacityProperty, new DoubleAnimation(0, 1, new TimeSpan(0, 0, 1)));
+                    }
+                }
+            }
         }
     }
 }
